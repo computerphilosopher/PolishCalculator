38f5b6c27f241713a7d9d44a43fb9999d13bf279
skynet <erlk0nig@naver.com>
2018-06-16 22:45:23 +0900

    주석 추가중

--- Calculator.h
+++ Calculator.h
@@ -24,7 +24,10 @@ enum Count {
 	SYMBOL_COUNT = 3
 };
 
-/* 제네릭한 변수를 담을 수 있는 스택 구현. 파일 분리가 쉽지 않아 헤더에 포함시킴*/
+/* 변수를 담을 수 있는 스택 구현. 파일 분리가 쉽지 않아 헤더에 포함시킴
+   템플릿으로 구현하였기 때문에 정수, 실수, 클래스 등을 모두 저장 가능
+
+*/
 template <class T>
 class Stack {
 
@@ -38,36 +41,60 @@ private:
 
 public:
 
-	Stack() {
+	Stack()
+	{
 		top = -1;
 	}
 
-	void push(T e) {
-		stack[++top] = e;
-	}
+	void push(T e)
+	{	
+		top++;
 
-	T pop() {
+		stack[top] = e;
+
+	}
+	
+	/*스택의 top을 제거하며 리턴*/
+	T pop()
+	{
 		return stack[top--];
 	}
 
-	T tos() {
+	/*스택의 top을 리턴(제거하지는 않음*/
+	T tos()
+	{
 		return stack[top];
 	}
 
-	bool empty() {
-		return top == -1;
+	/*스택이 비었는지 확인 */
+	bool empty()
+	{
+		if (top == -1) {
+			return true;
+		}
+		else {
+			return false;;
+		}
 	}
 
-	int size() {
+	/*스택의 크기 확인 */
+	int size()
+	{
 		return top + 1;
 	}
 };
 
 
+/* 스택에 담을 자료구조. 여러 자릿수의 수를 저장할 수 있도록 구현함*/
+
 class element {
+
 private:
 
+	//연산자, 피연산자, 괄호를 구분
 	int dataType;
+
+	//피연산자면 숫자의 값, 연산자나 괄호일 경우 종류를 저장j
 	int value;
 
 public:
@@ -75,6 +102,8 @@ public:
 	element(int dataType, int value);
 
 	element();
+
+	~element();
 
 	int GetType();
 
@@ -87,14 +116,25 @@ class PolishCalculator{
 
 private:
 
+	/*사용자가 입력한 스트링을 해석해서 element로 저장하는 큐 */
 	queue <element> parser;
+
+	/*사용자가 입력한 수식을 저장 */
 	string expression;
+
+	/*후위식으로 변환한 결과를 저장*/
 	queue <element> postfix;
+
+	/*후위식 계산에 쓰이는 스택*/
 	Stack <element> stack;
 
 public:
 
 	PolishCalculator(string expression);
+	
+	PolishCalculator();
+	
+	~PolishCalculator();
 
 	void ParsingExpression();
 
--- main.cpp
+++ main.cpp
@@ -15,16 +15,21 @@ int main()
 	cin >> exp;
 
 	PolishCalculator cal(exp);
+
 	cal.ParsingExpression();
 
 	cal.ToPostfix();
 
 	cal.Calculate();
 
+	/* 화면이 바로 꺼지는 것을 방지하기 위해 넣음. 없애도 무방*/
+
 	getchar();
+
 	getchar();
 
 }
+
 
 element::element(int dataType, int value)
 {
@@ -32,40 +37,61 @@ element::element(int dataType, int value)
 	this->value = value;
 }
 
+/*기본 생성자. 만약을 대비해 넣음*/
 element::element()
 {
 
 }
 
+/*기본 소멸자. 만약을 대비해 넣음*/
+element::~element()
+{
+
+}
+
+
+/* 연산자, 피연산자, 괄호 등의 타입 리턴*/
 int element::GetType()
 {
 	return dataType;
 }
 
+/* 피연산자일 경우 값, 연산자일 경우 종류 리턴, 괄호일 경우 왼쪽인지 오른쪽인지 리턴*/
 int element::GetValue()
 {
 	return value;
 }
 
+/*후위식 변환을 위해 우선순위 리턴*/
 int element::GetPriority()
 {
 	switch (value) {
+
+		/* 우선 순위는 숫자가 높을수록 높음*/
+
 	case LEFT_PAREN:
 		return 0;
 		break;
+
 	case ADD:
 		return 1;
 		break;
+
 	case SUB:
-		return 1;
+		return 2;
 		break;
+
 	case MUL:
-		return 2;
+		return 3;
 		break;
+
 	case DIV:
-		return 2;
+		return 4;
 		break;
+
 	}
+	
+	//아무 연산자도 아닐 경우 리턴
 	return -1;
 }
 
@@ -73,6 +99,16 @@ int element::GetPriority()
 PolishCalculator::PolishCalculator(string expression)
 {
 	this->expression = expression;
+}
+
+/*기본 생성자. 만약을 대비해 넣음*/
+PolishCalculator::PolishCalculator() {
+
+}
+
+/*기본 소멸자. 만약을 대비해 넣음*/
+PolishCalculator::~PolishCalculator() {
+
 }
 
 void PolishCalculator::ParsingExpression()
@@ -84,21 +120,27 @@ void PolishCalculator::ParsingExpression()
 		}
 		else {
 			switch (expression[i++]) {
+
 			case '+':
 				parser.push(element(OPERATOR, ADD));
 				break;
+
 			case '-':
 				parser.push(element(OPERATOR, SUB));
 				break;
+
 			case '*':
 				parser.push(element(OPERATOR, MUL));
 				break;
+
 			case '/':
 				parser.push(element(OPERATOR, DIV));
 				break;
+
 			case '(':
 				parser.push(element(PARENTHESES, LEFT_PAREN));
 				break;
+
 			case ')':
 				parser.push(element(PARENTHESES, RIGHT_PAREN));
 				break;
@@ -109,6 +151,7 @@ void PolishCalculator::ParsingExpression()
 
 }
 
+/**/
 int PolishCalculator::ParsingNum(int i)
 {
 	Stack<int> number;
@@ -118,17 +161,22 @@ int PolishCalculator::ParsingNum(int i)
 	}
 
 	int num = StackToInt(number);
+
 	parser.push(element(NUMBER, num));
 
 	return i;
 }
 
+/* 디버깅용 함수. 문자열을 파싱한 큐를 출력*/
 void PolishCalculator::printQueue()
 {
 	queue<element> temp = parser;
+
 	while (!temp.empty()) {
+
 		cout << temp.front().GetValue() << endl;
 		temp.pop();
+
 	}
 }
 
@@ -136,12 +184,15 @@ void PolishCalculator::ToPostfix()
 {
 
 	while (!parser.empty()) {
+
 		element e = parser.front();
 
 		switch (e.GetType()) {
+
 		case NUMBER:
 			postfix.push(e);
 			break;
+
 		case PARENTHESES:
 			if (e.GetValue() == LEFT_PAREN) {
 				stack.push(e);
@@ -149,103 +200,162 @@ void PolishCalculator::ToPostfix()
 
 			//오른쪽 괄호일 때
 			else {
+
 				element temp = stack.tos();
+
+				/*왼쪽 괄호가 나올때까지 계속 pop한다*/
+
 				while (temp.GetValue() != LEFT_PAREN) {
+
 					temp = stack.pop();
+
 					postfix.push(temp);
+
 				}
+
 			}
+
 			break;
+
 		case OPERATOR:
+
 			element temp = stack.tos();
+
+			/* 우선 순위 낮은 연산자가 나올때까지 계속 pop한다*/
 			while (temp.GetPriority() > e.GetPriority()) {
+
 				temp = stack.pop();
+
 				postfix.push(temp);
+
 			}
+
+			/*pop이 끝나면 해당 연산자를 스택에 넣음*/
 			stack.push(e);
+
 			break;
 		}
+
 		parser.pop();
+
 	}
 
+	/* 나머지 남아있는 연산자를 푸쉬함*/
 	while (!stack.empty()) {
+
 		postfix.push(stack.pop());
+
 	}
 }
 
+/*1의 자리부터 순서대로 푸쉬한 스택을 int형으로 바꾸어주는 메소드*/
 int PolishCalculator::StackToInt(Stack<int> &number)
 {
 	int digit = 1;
+
 	int ret = 0;
 
+	/*1의 자리 * 1, 10의 자리 * 10, 100자리 * 100 ....을 반복하면서 더함*/
 	while (!number.empty()) {
+
 		ret += number.pop() * digit;
+
 		digit *= 10;
+
 	}
+
 	return ret;
+
 }
 
 void PolishCalculator::Calculate()
 {
 	Stack <double> stack;
 
+	/* 후위식 큐가 끝날때까지 루프 반복*/
 	while (!postfix.empty()) {
+
 		element e = postfix.front();
 
 		int type = e.GetType();
+
 		int value = e.GetValue();
 
 		double result = 0.0;
+
+		/*피연산자가 나오면 바로 푸쉬*/
 		if (type == NUMBER) {
+
 			stack.push(value);
+
 		}
 
+		/*연산자가 나오면 두 숫자를 차례로 pop해서 연산한 뒤 결과를 스택에 넣음*/
 		else if (type == OPERATOR) {
+
 			double num1 = stack.pop();
+
 			double num2 = stack.pop();
 
 			switch (value) {
+
 			case ADD:
 				stack.push(num1 + num2);
 				break;
+
 			case SUB:
-				stack.push(num1 - num2);
+				stack.push(num2 - num1);
 				break;
+
 			case MUL:
 				stack.push(num1 * num2);
 				break;
+
 			case DIV:
-				stack.push(num1 * num2);
+				stack.push(num2 / num1);
 				break;
 			}
 		}
+
 		postfix.pop();
+
 	}
+
 	cout << stack.tos();
+
 }
 
+/*디버깅용 출력 함수. 후위식으로 바꾼 결과를 출력함*/
 void PolishCalculator::PrintPostfix()
 {
 	queue <element> temp = postfix;
 
 	cout << "postfix:";
+
 	while (!temp.empty()) {
+
 		element e = temp.front();
 
 		int type = e.GetType();
+
 		int value = e.GetValue();
 
 		if (type == OPERATOR) {
+
 			switch (value) {
+
 			case ADD:
 				cout << "+";
 				break;
+
 			case SUB:
 				cout << "-";
 				break;
+
 			case MUL:
 				cout << "*";
 				break;
+
 			case DIV:
 				cout << "/";
 				break;
@@ -255,10 +365,13 @@ void PolishCalculator::PrintPostfix()
 		else {
 			cout << value;
 		}
+
 		temp.pop();
+
 		cout << " ";
 
 	}
+
 	cout << endl;
 }
 
